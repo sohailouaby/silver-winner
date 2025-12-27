dd1648fb3f0b1bdd829cbf9d4b816f6667bb09d7
BentJoker19 <sohailouaby23@gmail.com>
2025-12-27 15:24:24 +0100

    Add MyScript.cs with basic Unity functionality

---
+++ Assets/Scripts/MyScript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello from Unity!");
+    }
+
+    void Update()
+    {
+        //Debug.Log("Hello man");
+    }
+}
